4ae2bea2ff03a32b5bc9f2b10da5567a2c35c08e
alison <tmrfandom@gmail.com>
2023-10-04 21:25:52 -0700

    add chakra-dayzed-datepicker

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,7 +1,49 @@
-import { Text } from "@chakra-ui/react";
+import {Heading} from "@chakra-ui/react";
+import { RangeDatepicker } from "chakra-dayzed-datepicker";
+import { useState } from "react";
+import {VStack} from "@chakra-ui/react";
 
 export default function New(): JSX.Element {
-    return(
-        <Text>Create a New Meeting</Text>
+    const [selectedDates, setSelectedDates] = useState<Date[]>([new Date(), new Date()]);
+
+    return (
+        <div>
+            <VStack>
+            <Heading>Create a New Meeting</Heading>
+            <RangeDatepicker
+                selectedDates={selectedDates}
+                onDateChange={setSelectedDates}
+                propsConfigs={{
+                    dateNavBtnProps: {
+                        colorScheme: 'blue',
+                        variant: 'outline',
+                    },
+                    dayOfMonthBtnProps: {
+                        defaultBtnProps: {
+                            borderColor: 'red.300',
+                            _hover: {
+                                background: 'blue.400',
+                            },
+                        },
+                        isInRangeBtnProps: {
+                            color: 'purple.800',
+                            borderColor: 'blue.300',
+                        },
+                        selectedBtnProps: {
+                            background: 'blue.200',
+                            borderColor: 'blue.300',
+                            color: 'blue.600',
+                        },
+                        todayBtnProps: {
+                            background: 'teal.200',
+                            color: 'teal.700',
+                        },
+                    },
+                    inputProps: {
+                        size: 'sm',
+                    },
+                }}/>
+            </VStack>
+        </div>
     )
 }
