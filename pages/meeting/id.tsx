cd5889721685f5779e7554e869a25de687762ba8
alison <tmrfandom@gmail.com>
2023-10-09 17:56:00 -0700

    add new meeting page

---
+++ pages/meeting/id.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Navbar from "../../components/navbar";
+import * as React from "react";
+
+export default function Meeting() {
+    return <>{
+        <div >
+            <Navbar/>
+
+        </div>
+    }</>;
+}
