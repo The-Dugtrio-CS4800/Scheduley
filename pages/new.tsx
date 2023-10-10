e9b12dc97f21bf69f1a170d351fadf55336f6a18
alison <tmrfandom@gmail.com>
2023-10-09 17:55:51 -0700

    reformat new page code and add navbar

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,37 +1,41 @@
-import {Button, Heading, Link} from "@chakra-ui/react";
-import { RangeDatepicker } from "chakra-dayzed-datepicker";
-import {VStack} from "@chakra-ui/react";
-import React, { useState } from "react";
-import DatePicker, {Calendar, DateObject} from "react-multi-date-picker"
-import type{Value} from "react-multi-date-picker"
+import {Button, Heading, Link, VStack} from "@chakra-ui/react";
+import React, {useState} from "react";
+import {Calendar, DateObject} from "react-multi-date-picker"
 import NextLink from "next/link";
+import Navbar from "../components/navbar";
 
-export default function New(): JSX.Element {
+export default function New() {
     const [selectedDates, setSelectedDates] = useState<Date[]>([new Date(), new Date()]);
     const [values, setValues] = useState([
-        [new DateObject().set({ day: 1 }), new DateObject().set({ day: 3 })],
-        [new DateObject().set({ day: 6 }), new DateObject().set({ day: 12 })],
-        [new DateObject().set({ day: 23 }), new DateObject().set({ day: 27 })],
+        [new DateObject().set({day: 1}), new DateObject().set({day: 3})],
+        [new DateObject().set({day: 6}), new DateObject().set({day: 12})],
+        [new DateObject().set({day: 23}), new DateObject().set({day: 27})],
     ])
 
 
-    return (
-        <div>
-            <VStack>
+    return (<>
+
+        <Navbar/>
+        <VStack>
             <Heading>Create a New Meeting</Heading>
-                <Calendar
-                    value={values}
-                    onChange={setValues}
-                    multiple
-                    range
-                />
-                <Link as={NextLink} href='/meeting'>
+            <Calendar
+                value={values}
+                onChange={setValues}
+                multiple
+                range
+            />
+            <Link as={NextLink} href='/meeting/id'>
                 <Button colorScheme='teal' variant='solid'
-                onClick = {()=> {values.map((dateList)=>{dateList.map((date)=>{console.log(date.format())})})}}>
+                        onClick={() => {
+                            values.map((dateList) => {
+                                dateList.map((date) => {
+                                    console.log(date.format())
+                                })
+                            })
+                        }}>
                     Submit
                 </Button>
-                </Link>
-            </VStack>
-        </div>
-    )
+            </Link>
+        </VStack>
+    </>)
 }
