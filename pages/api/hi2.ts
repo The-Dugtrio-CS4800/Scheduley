55c36785d8ad8f16cd386b46d0236793be44ab69
CleoY <77142560+CleoY@users.noreply.github.com>
2023-10-25 14:37:22 -0700

    Changed msg to see if GitHub Actions will automatically deploy web service when a change is pushed.

--- pages/api/hi2.ts
+++ pages/api/hi2.ts
@@ -5,5 +5,5 @@ type Data = {
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    res.status(200).json({ message: 'If dugtrio is 50/50 female/male, does that mean one of the heads is intersex?' })
+    res.status(200).json({ message: 'Testing to see if GitHub Actions works.' })
   }
