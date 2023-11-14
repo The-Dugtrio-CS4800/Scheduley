0a3701f95e31a0e8801980dc8e7d24eaf2048283
CleoY <77142560+CleoY@users.noreply.github.com>
2023-11-13 22:33:32 -0800

    x5

--- pages/api/hi2.ts
+++ pages/api/hi2.ts
@@ -5,5 +5,5 @@ type Data = {
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    res.status(200).json({ message: 'Testing to see if GitHub Actions works AGAIN x4.' })
+    res.status(200).json({ message: 'Testing to see if GitHub Actions works AGAIN x5.' })
   }
