c5ba2bbb55ac00b50e1936cc783eacdd3388feb5
erikajledesma <erika.jasmine06@gmail.com>
2023-11-26 18:12:45 -0800

    wip added login page components

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -1,46 +1,83 @@
-import { Input, Stack, Heading, InputGroup, InputLeftAddon, InputRightAddon, InputRightElement, Button} from '@chakra-ui/react'
-import React, {useState} from "react";
-import Link from 'next/link'
+// import { Input, Stack, Heading, InputGroup, InputLeftAddon, InputRightAddon, InputRightElement, Button} from '@chakra-ui/react'
+// import React, {useState} from "react";
+import App from "../components/App";
+// import Link from 'next/link'
 import Navbar from "../components/navbar";
+
+import {
+    Box,
+    Button,
+    Checkbox,
+    Container,
+    Divider,
+    FormControl,
+    FormLabel,
+    Heading,
+    HStack,
+    Input,
+    Link,
+    Stack,
+    Text,
+    Image
+  } from '@chakra-ui/react'
+  import { Logo } from '../components/Logo'
+  import { OAuthButtonGroup } from '../components/OAuthButtonGroup'
+  import { PasswordField } from '../components/PasswordField'
 
 
 export default function SignIn() {
 
-    // function PasswordInput() {
-    //     const [show, setShow] = React.useState(false)
-    //     const handleClick = () => setShow(!show)
-
     return (<>
 
         <Navbar/>
-        <Stack spacing={4}>
-            <InputGroup>
-                <InputLeftAddon children='Username' />
-                <Input placeholder='jsmith' />
-            </InputGroup>
-
-            {/* <InputGroup size='md'>
-                <Input
-                    pr='4.5rem'
-                    type={show ? 'text' : 'password'}
-                    placeholder='Enter password'
-                />
-                <InputRightElement width='4.5rem'>
-                    <Button h='1.75rem' size='sm' onClick={handleClick}>
-                    {show ? 'Hide' : 'Show'}
-                    </Button>
-                </InputRightElement>
-            </InputGroup> */}
-
-            {/* If you add the size prop to `InputGroup`, it'll pass it to all its children. */}
-            <InputGroup size='sm'>
-                <InputLeftAddon children='https://' />
-                <Input placeholder='mysite' />
-                <InputRightAddon children='.com' />
-            </InputGroup>
-        </Stack>    
+        <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
+      <Stack spacing="8">
+        <Stack spacing="6">
+          <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+          {/* <Image src="/ScheduleyLogo.png" alt="Logo" height="200" width="auto" objectFit="contain" /> */}
+            <Heading size={{ base: 'xs', md: 'sm' }}>Log in to your account</Heading>
+            <Text color="fg.muted">
+              Don't have an account? <Link href="#">Sign up</Link>
+            </Text>
+          </Stack>
+        </Stack>
+        <Box
+          py={{ base: '0', sm: '8' }}
+          px={{ base: '4', sm: '10' }}
+          bg={{ base: 'transparent', sm: 'bg.surface' }}
+          boxShadow={{ base: 'none', sm: 'md' }}
+          borderRadius={{ base: 'none', sm: 'xl' }}
+        >
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" type="email" />
+              </FormControl>
+              <PasswordField />
+            </Stack>
+            <HStack justify="space-between">
+              <Checkbox defaultChecked>Remember me</Checkbox>
+              <Button variant="text" size="sm">
+                Forgot password?
+              </Button>
+            </HStack>
+            <Stack spacing="6">
+              <Button>Sign in</Button>
+              <HStack>
+                <Divider />
+                <Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
+                  or continue with
+                </Text>
+                <Divider />
+              </HStack>
+              <OAuthButtonGroup />
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
     </>)
-    // }
     
 }
 
