8240bdb2372ca9c547cc61ac9319332bcc012940
alison <tmrfandom@gmail.com>
2023-11-28 13:39:42 -0800

    add setup to post meeting object

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,26 +1,46 @@
 import {Button, Heading, VStack} from "@chakra-ui/react";
 import React, {useState} from "react";
 import {Calendar, DateObject} from "react-multi-date-picker"
-import Link from 'next/link'
-import Navbar from "../components/navbar";
 
-function generateID(){
-    const id = 234
-    return id;
+import Navbar from "../components/navbar";
+import {useRouter} from "next/router";
+
+async function generateID() {
+    try {
+        const response = await fetch("http://localhost:8080/meeting", {
+            method: "POST", // or 'PUT'
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: "name"
+            }),
+        });
+
+        const result = await response.json();
+        console.log("Success:", result);
+        return 234
+    } catch (error) {
+        console.error("Error:", error);
+        return 999
+    }
+
 }
 
 export default function New() {
+    const router = useRouter()
     const oneWeek = new DateObject()
     oneWeek.day += 6
     const [dates, setDates] = useState([
         [new DateObject(), oneWeek],
     ])
+    let id;
 
     return (<>
 
         <Navbar/>
         <VStack spacing='5'>
-            
+
             <Heading py={{ base: '6', md: '12' }}>Create a New Meeting</Heading>
             <Calendar
                 // @ts-ignore
@@ -30,19 +50,20 @@ export default function New() {
                 multiple
                 range
             />
-                <Link href={`/meeting/${encodeURIComponent(generateID())}`}>
+
                 <Button variant='solid' color='white' data-cy='submit' bg={'#c14953'}
-                        onClick={() => {
+                        onClick={async () => {
                             dates.map((dateList) => {
                                 dateList.map((date) => {
                                     console.log(date.format())
                                 })
                             })
-                            generateID()
+                            id = await generateID();
+                            router.push(`/meeting/${encodeURIComponent(id)}`)
                         }}>
                     Submit
                 </Button>
-                </Link>
+
         </VStack>
     </>)
 }
