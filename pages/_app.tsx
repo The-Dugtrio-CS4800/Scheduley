01da2955656bb18ad89baf0b2ca583bbd32b1a44
CleoY <77142560+CleoY@users.noreply.github.com>
2023-11-29 21:58:33 -0800

    Reverted _app.tsx file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,33 +4,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react';
 import { Box } from "@chakra-ui/react"
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from '../components/navbar';
 
-// function MyApp({ Component, pageProps }) {
-//     return (
-//         // <Providers>
-//         <ChakraProvider>
-//             <Component {...pageProps} />
-//             {/* <Box w="100%" h="1000px" bgGradient="radial(red.300, yellow.400, pink.200)" /> */}
-//             {/* <Box w="100%" h="1000px" bgColor="#F9F5F0" /> */}
-//         </ChakraProvider>
-//         // </Providers>
-//     )
-// }
-
-const MyApp = () => {
+function MyApp({ Component, pageProps }) {
     return (
         // <Providers>
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/other" component={OtherPage} />
-            </Switch>
-        </Router>
-        
+        <ChakraProvider>
+            <Component {...pageProps} />
+            {/* <Box w="100%" h="1000px" bgGradient="radial(red.300, yellow.400, pink.200)" /> */}
+            {/* <Box w="100%" h="1000px" bgColor="#F9F5F0" /> */}
+        </ChakraProvider>
         // </Providers>
     )
 }
