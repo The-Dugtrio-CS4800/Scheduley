b493f5ade3ebbe5140ddde8e26f9302ee332ab4b
CleoY <77142560+CleoY@users.noreply.github.com>
2023-11-29 21:57:55 -0800

    Revert "Moved meeting and poll buttons/images to index.tsx (home page)."
    This reverts commit 6e58531a96cd5c393bce916e999e2d8fbfa5a729.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "../components/navbar";
 
-import MyApp from './_app'; // The root component of your application
-
-// ReactDOM.render(
-//   <Router>
-//     <MyApp />
-//   </Router>,
-//   document.getElementById('root')
-// );
-
 export default function Home(): JSX.Element {
     return (
         <>
