9d94095ce627f91d89ca4b91592484ad2488eb24
CleoY <77142560+CleoY@users.noreply.github.com>
2023-11-29 21:54:47 -0800

    Revert "Moved meeting and poll buttons/images to index.tsx (home page)."
    This reverts commit 6e58531a96cd5c393bce916e999e2d8fbfa5a729.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -150,6 +150,85 @@ const Navbar = () => {
                     </Box>
                 ) : null}
             </Box>
+            {/* <Flex h={20} alignItems={'center'} justifyContent={'center'}>
+                <img
+                    src="meeting.png"
+                    alt="Image Alt Text"
+                    style={{ marginBottom: '10px', width: '250px', height: 'auto' }}
+                />
+                
+                <Link as={NextLink} href='/new'>
+                    <Button color='white'
+                        variant={'solid'}
+                        bg={'#c14953'}
+                        size={'sm'}
+                        mr={4}
+                        leftIcon={<AddIcon />}>
+                        New Meeting
+                    </Button>
+                </Link>
+                <Link as={NextLink} href='/poll'>
+                <Button color='white'
+                    variant={'solid'}
+                    bg={'#c14953'}
+                    size={'sm'}
+                    mr={4}
+                    leftIcon={<AddIcon/>}>
+                    New Poll
+                </Button>
+                </Link>
+            </Flex> */}
+
+            <div style={{ 
+            display: 'flex', 
+            flexDirection: 'row', 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            height: 'calc(100vh - heightOfNavbar)', // Replace heightOfNavbar with the actual height of your navbar
+            boxSizing: 'border-box',
+            paddingTop: 'heightOfNavbar' // Replace heightOfNavbar with the actual height of your navbar
+            }}>
+            <div style={{ 
+                display: 'flex', 
+                flexDirection: 'column', 
+                alignItems: 'center', 
+                justifyContent: 'center',
+                margin: '0 10px' // Adds spacing between the combos
+            }}>
+                <img src="meeting.png" alt="New meeting sample" style={{ display: 'block', width: '250px', height: 'auto', marginBottom: '10px' }} />
+                <Link as={NextLink} href='/new'>
+                    <Button color='white'
+                        variant={'solid'}
+                        bg={'#c14953'}
+                        size={'sm'}
+                        mr={4}
+                        leftIcon={<AddIcon />}>
+                        New Meeting
+                    </Button>
+                </Link>
+            </div>
+            <div style={{ 
+                display: 'flex', 
+                flexDirection: 'column', 
+                alignItems: 'center', 
+                justifyContent: 'center',
+                margin: '0 10px' // Adds spacing between the combos
+            }}>
+                <img src="meeting.png" alt="New poll sample" style={{ display: 'block', width: '250px', height: 'auto', marginBottom: '10px' }} />
+                <Link as={NextLink} href='/poll'>
+                <Button color='white'
+                    variant={'solid'}
+                    bg={'#c14953'}
+                    size={'sm'}
+                    mr={4}
+                    leftIcon={<AddIcon/>}>
+                    New Poll
+                </Button>
+                </Link>
+            </div>
+            </div>
+
+
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,6 @@ import * as React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "../components/navbar";
-import {
-    Button,
-    Link,
-} from '@chakra-ui/react'
-import NextLink from 'next/link'
-import {AddIcon } from '@chakra-ui/icons'
 
 import MyApp from './_app'; // The root component of your application
 
@@ -28,54 +22,6 @@ export default function Home(): JSX.Element {
             </Head>
 
             <Navbar/>
-            <div style={{ 
-            display: 'flex', 
-            flexDirection: 'row', 
-            alignItems: 'center', 
-            justifyContent: 'center', 
-            height: 'calc(100vh - heightOfNavbar)', // Replace heightOfNavbar with the actual height of your navbar
-            boxSizing: 'border-box',
-            paddingTop: 'heightOfNavbar' // Replace heightOufNavbar with the actual height of your navbar
-            }}>
-            <div style={{ 
-                display: 'flex', 
-                flexDirection: 'column', 
-                alignItems: 'center', 
-                justifyContent: 'center',
-                margin: '0 10px' // Adds spacing between the combos
-            }}>
-                <img src="meeting.png" alt="New meeting sample" style={{ display: 'block', width: '250px', height: 'auto', marginBottom: '10px' }} />
-                <Link as={NextLink} href='/new'>
-                    <Button color='white'
-                        variant={'solid'}
-                        bg={'#c14953'}
-                        size={'sm'}
-                        mr={4}
-                        leftIcon={<AddIcon />}>
-                        New Meeting
-                    </Button>
-                </Link>
-            </div>
-            <div style={{ 
-                display: 'flex', 
-                flexDirection: 'column', 
-                alignItems: 'center', 
-                justifyContent: 'center',
-                margin: '0 10px' // Adds spacing between the combos
-            }}>
-                <img src="meeting.png" alt="New poll sample" style={{ display: 'block', width: '250px', height: 'auto', marginBottom: '10px' }} />
-                <Link as={NextLink} href='/poll'>
-                <Button color='white'
-                    variant={'solid'}
-                    bg={'#c14953'}
-                    size={'sm'}
-                    mr={4}
-                    leftIcon={<AddIcon/>}>
-                    New Poll
-                </Button>
-                </Link>
-            </div>
-            </div>
         </>
     );
 }
