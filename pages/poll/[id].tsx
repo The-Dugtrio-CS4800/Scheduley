2eb3875e45e03bd314feaa61be3f81d8e7245338
CleoY <77142560+CleoY@users.noreply.github.com>
2023-12-01 15:13:06 -0800

    Trying to test amazon link vs. local link.

--- pages/poll/[id].tsx
+++ pages/poll/[id].tsx
@@ -1,40 +1,74 @@
 import {useRouter} from 'next/router'
 import Navbar from "../../components/navbar";
 import * as React from "react";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ScheduleSelector from 'react-schedule-selector'
-import {Box, Button, VStack} from "@chakra-ui/react";
+import ScheduleViewer from "../../components/scheduleViewer";
+import {Button, HStack, VStack} from "@chakra-ui/react";
 
 export default function Meeting() {
     const router = useRouter()
     const [schedule, setSchedule] = useState([])
+    const [scheduleViewer, setScheduleViewer] = useState([])
+    const [participants, setParticipants] = useState([])
+
+
+    useEffect(() => {
+        console.log(scheduleViewer);
+    }, [scheduleViewer])
+
+    function updateSchedule(){
+        participants.push(
+            {
+                name: "name",
+                schedule: schedule,
+            }
+        )
+    }
 
     return <>{
+
         <div>
             <Navbar/>
             {router.query.id}
-            <Box m={25}>
             <VStack >
-                <ScheduleSelector
-                    selection={schedule}
-                    numDays={5}
-                    minTime={10}
-                    maxTime={22}
-                    hourlyChunks={2}
-                    timeFormat="h:mma"
-                    onChange={setSchedule}
-                />
-                <Button
-                    onClick={() => {
-                        (schedule.map((date) => {
-                            console.log(date)
-                        }))
-                    }}
-                >
-                    Submit
-                </Button>
+                <HStack
+                spacing ={50}>
+                    <ScheduleSelector
+                        selection={schedule}
+                        numDays={5}
+                        minTime={8}
+                        maxTime={22}
+                        hourlyChunks={2}
+                        timeFormat="h:mma"
+                        onChange={setSchedule}
+                    />
+                    <ScheduleViewer
+                        selection={scheduleViewer}
+                        selectionScheme={'square'}
+                        numDays={5}
+                        minTime={8}
+                        maxTime={22}
+                        hourlyChunks={2}
+                        timeFormat="h:mma"
+                        onChange={setScheduleViewer}
+                        hoveredColor={'none'}
+                        selectedColor={'blue'}
+                        unselectedColor={'purple'}
+                        participants={participants}
+                    />
+                    <Button
+                        padding={5}
+                        margin={5}
+                        onClick={()=>{updateSchedule()
+                            setScheduleViewer(schedule)
+                            setSchedule([])}}
+                    >
+                        Add availability
+                    </Button>
+                </HStack>
+
             </VStack>
-            </Box>
         </div>
     }</>;
 }
