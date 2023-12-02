3388b08eb616cbd064756668c01eee8f83b85a62
erikajledesma <erika.jasmine06@gmail.com>
2023-12-02 13:24:13 -0800

    add signIn to gitHub icon

--- components/OAuthButtonGroup.tsx
+++ components/OAuthButtonGroup.tsx
@@ -1,16 +1,17 @@
 import { Button, ButtonGroup, VisuallyHidden } from '@chakra-ui/react'
 import { GitHubIcon, GoogleIcon, TwitterIcon } from './ProviderIcons'
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const providers = [
-  { name: 'Google', icon: <GoogleIcon /> },
-  { name: 'Twitter', icon: <TwitterIcon /> },
+  // { name: 'Google', icon: <GoogleIcon /> },
+  // { name: 'Twitter', icon: <TwitterIcon /> },
   { name: 'GitHub', icon: <GitHubIcon /> },
 ]
 
 export const OAuthButtonGroup = () => (
   <ButtonGroup variant="secondary" spacing="4">
     {providers.map(({ name, icon }) => (
-      <Button key={name} flexGrow={1}>
+      <Button onClick={() => signIn()} key={name} flexGrow={1}>
         <VisuallyHidden>Sign in with {name}</VisuallyHidden>
         {icon}
       </Button>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import '../styles/globals.css'
-// import { Providers } from "./auth/Providers";
+// import '../styles/globals.css'
+// import { Providers } from "./api/auth/Providers";
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react';
 import { Box } from "@chakra-ui/react"
 
-function MyApp({ Component, pageProps }) {
+
+function MyApp({ Component, pageProps: { session, ...pageProps }}) {
     return (
-        // <Providers>
         <ChakraProvider>
+            <SessionProvider session={session}>
             <Component {...pageProps} />
+            </SessionProvider>
             {/* <Box w="100%" h="1000px" bgGradient="radial(red.300, yellow.400, pink.200)" /> */}
             {/* <Box w="100%" h="1000px" bgColor="#F9F5F0" /> */}
         </ChakraProvider>
-        // </Providers>
     )
 }
 
