fef0f4071530b641ef037152d9e85893d3d835a0
alison <tmrfandom@gmail.com>
2023-12-02 22:39:44 -0800

    add proper styled component support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,22 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react';
 import { Box } from "@chakra-ui/react"
+import App from 'next/app'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {}
 
 
 function MyApp({ Component, pageProps: { session, ...pageProps }}) {
     return (
         <ChakraProvider>
+            <ThemeProvider theme={theme}>
             <SessionProvider session={session}>
             <Component {...pageProps} />
             </SessionProvider>
             {/* <Box w="100%" h="1000px" bgGradient="radial(red.300, yellow.400, pink.200)" /> */}
             {/* <Box w="100%" h="1000px" bgColor="#F9F5F0" /> */}
+            </ThemeProvider>
         </ChakraProvider>
     )
 }
