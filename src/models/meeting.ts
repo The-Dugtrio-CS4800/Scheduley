95d2f0013d08dd17e521dea751a3b7fbb3289691
alison <tmrfandom@gmail.com>
2023-12-02 22:40:31 -0800

    add participant type to meeting model

--- src/models/meeting.ts
+++ src/models/meeting.ts
@@ -1,7 +1,12 @@
 // External dependencies
 import { ObjectId } from "mongodb";
 
+type Participant = {
+    name: string
+    schedule: Date[]
+}
+
 // Class Implementation
 export default class Meeting {
-    constructor(public name: string, public title: string, public participants: String[], public dates: Date[], public id?: ObjectId) {}
+    constructor(public name: string, public title: string, public participants: Participant[], public dates: Date[], public id?: ObjectId) {}
 }
